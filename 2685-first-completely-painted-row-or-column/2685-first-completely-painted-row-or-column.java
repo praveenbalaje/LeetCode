305ab94ff527fa57edbd446db754860edaf27f0b
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-01-20 14:07:21 +0530

    Time: 3 ms (99.5%), Space: 66.1 MB (55.72%) - LeetHub

---
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        int index = -1;
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
+        int[] position = new int[(m * n) + 1];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int value = (i * n) + j;
+                position[mat[i][j]] = value;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int value = arr[i];
+            int pos = position[value];
+            row[pos / n] += 1;
+            col[pos % n] += 1;
+
+            if(row[pos/n] == col.length || col[pos%n] == row.length) {
+                index = i;
+                break;
+            }
+            // if ((i+1 >= n || i+1 >= m) && checkCellPainted(row, col)) {
+            //     index = i;
+            //     break;
+            // }
+        }
+
+        return index;
+    }
+
+    private boolean checkCellPainted(int[] row, int[] col) {
+        boolean painted = false;
+
+        for (int i = 0; i < row.length; i++) {
+            if (col.length == row[i]) {
+                painted = true;
+                break;
+            }
+        }
+
+        if (!painted) {
+            for (int i = 0; i < col.length; i++) {
+                System.out.println(col[i] + "  " + col.length);
+                if (row.length == col[i]) {
+                    painted = true;
+                    break;
+                }
+            }
+        }
+        return painted;
+
+    }
+}
