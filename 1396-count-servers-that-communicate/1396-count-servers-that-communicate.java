fb8ca010a7b737beea39018ae064e2ca92621b8e
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-01-23 08:25:32 +0530

    Time: 2 ms (100%), Space: 52.7 MB (27.79%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -1,48 +1,33 @@
 class Solution {
     public int countServers(int[][] grid) {
-        int col[] = new int[grid[0].length];
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        int count = 0;
+        // Arrays to count the number of servers in each row and column
+        int[] rowCount = new int[rows];
+        int[] colCount = new int[cols];
 
-        for (int i = 0; i < grid.length; i++) {
-
-            int previousRow = -1;
-            for (int j = 0; j < grid[0].length; j++) {
+        // First pass: Count the number of servers in each row and column
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
                 if (grid[i][j] == 1) {
-                    if (previousRow == -1) {
-                        previousRow = j;
-                    } else {
-                        if (grid[i][previousRow] == 1) {
-                            count += 2;
-                        } else {
-                            count += 1;
-                        }
-
-                        col[previousRow] = 2;
-                        grid[i][previousRow] = 2;
-                        grid[i][j] = 2;
-                        previousRow = j;
-                    }
-                }
-
-                if (grid[i][j] != 0) {
-
-                    if (grid[i][j] == 1 && col[j] == 1) {
-                        count += 2;
-                        grid[i][j] = 2;
-                        col[j] = 2;
-                    } else if (grid[i][j] == 2 && col[j] == 1) {
-                        count += 1;
-                        col[j] = 2;
-                    } else if (grid[i][j] == 1 && col[j] == 2) {
-                        count += 1;
-                        grid[i][j] = 2;
-                    } else {
-                        col[j] = grid[i][j];
-                    }
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
             }
         }
+
+        int count = 0;
+
+        // Second pass: Count servers that can communicate
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1)) {
+                    count++;
+                }
+            }
+        }
+
         return count;
     }
 }
