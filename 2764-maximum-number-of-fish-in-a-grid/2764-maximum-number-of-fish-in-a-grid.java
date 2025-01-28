43fcb40fef9450ca49384f4aa43c0e0a2a518e24
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-01-28 10:46:05 +0530

    Time: 3 ms (100%), Space: 44.5 MB (98.6%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.java
@@ -1,38 +1,37 @@
 class Solution {
     public int findMaxFish(int[][] grid) {
         int res = 0;
-        for(int i=0;i<grid.length;i++) {
-            for(int j=0;j<grid[0].length;j++) {
-                if(grid[i][j] !=0) {
-                    int sum = visitLand(grid,0,i,j);
-                    if(sum > res) {
-                        res = sum;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0) { // Start DFS on water cells
+                    int sum = visitWater(grid, 0, i, j);
+                    if (sum > res) {
+                        res = sum; // Update the maximum fish count
                     }
                 }
-
             }
         }
-
         return res;
     }
 
-    private int visitLand(int[][] grid,int count,int i, int j) {
-        if(grid[i][j] == 0) {
+    private int visitWater(int[][] grid, int count, int i, int j) {
+        if (grid[i][j] == 0) { // Base case: water or visited cell
             return count;
         } else {
-            count += grid[i][j];
-            grid[i][j] = 0;
-            if(i+1 < grid.length && grid[i+1][j] != 0) {
-                count = visitLand(grid,count,i+1,j);
-            } 
-            if(j+1 < grid[0].length && grid[i][j+1] != 0) {
-                count = visitLand(grid,count,i,j+1);
+            count += grid[i][j]; // Add fish count
+            grid[i][j] = 0; // Mark as visited
+            // Explore adjacent cells
+            if (i + 1 < grid.length) {
+                count = visitWater(grid, count, i + 1, j);
             }
-            if(i-1 >= 0 && grid[i-1][j] != 0) {
-                count = visitLand(grid,count,i-1,j);
-            } 
-            if(j-1 >=0 && grid[i][j-1] != 0) {
-                count = visitLand(grid,count,i,j-1);
+            if (j + 1 < grid[0].length) {
+                count = visitWater(grid, count, i, j + 1);
+            }
+            if (i - 1 >= 0) {
+                count = visitWater(grid, count, i - 1, j);
+            }
+            if (j - 1 >= 0) {
+                count = visitWater(grid, count, i, j - 1);
             }
         }
         return count;
