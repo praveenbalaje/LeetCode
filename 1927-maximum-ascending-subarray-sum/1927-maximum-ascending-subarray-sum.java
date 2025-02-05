d2bc78a9ac64aca2584089a431ad25eb9704f789
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-02-05 10:38:23 +0530

    Time: 0 ms (100%), Space: 41.1 MB (83.11%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -1,25 +1,20 @@
 class Solution {
 
     public int maxAscendingSum(int[] nums) {
-        int maxSum = 0;
-
-        // Outer loop to start from each element in the array
-        for (int startIdx = 0; startIdx < nums.length; startIdx++) {
-            int currentSubarraySum = nums[startIdx];
-
-            // Inner loop to check the next elements forming an ascending subarray
-            for (
-                int endIdx = startIdx + 1;
-                endIdx < nums.length && nums[endIdx] > nums[endIdx - 1];
-                endIdx++
-            ) {
-                currentSubarraySum += nums[endIdx];
-            }
-
-            // Update maxSum if we find a larger ascending subarray sum
-            maxSum = Math.max(maxSum, currentSubarraySum);
+        int sum = 0;
+        int count = 0;
+        for(int i=0;i<nums.length;i++) {
+            count += nums[i];
+            if(i+1<nums.length && nums[i] >= nums[i+1]) {
+                if(count > sum) {
+                    sum = count;
+                }
+                count = 0;
+            } 
         }
-
-        return maxSum;
+         if(count > sum) {
+            sum = count;
+         }
+        return sum;
     }
 }
