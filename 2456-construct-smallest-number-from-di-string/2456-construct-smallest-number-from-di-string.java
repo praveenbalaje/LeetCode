b6f0f1665dd9915ee173903c6b4e1b726363629b
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-02-18 12:44:20 +0530

    Time: 1 ms (75.25%), Space: 41.4 MB (96.23%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,21 +1,50 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public String smallestNumber(String pattern) {
-        StringBuilder result = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        
-        for (int i = 0; i <= pattern.length(); i++) {
-            stack.push(i + 1); // Push numbers from 1 to n+1
+        int ic = 0, dc = 0;
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        StringBuilder res = new StringBuilder();
+        boolean addedLast = false;
 
-            if (i == pattern.length() || pattern.charAt(i) == 'I') {
-                // Pop all elements from the stack when encountering 'I' or at the end
-                while (!stack.isEmpty()) {
-                    result.append(stack.pop());
+        // Count 'I' and 'D' occurrences
+        for (char ch : pattern.toCharArray()) {
+            if (ch == 'I') ic++;
+            else dc++;
+        }
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            if (ch == 'I') {
+                res.append(list.remove(0));
+                ic--;
+
+                if (ic == 0 && dc == 0 && !addedLast) {
+                    addedLast = true;
+                    res.append(list.remove(0));
+                }
+            } else { // 'D' case
+                if (ic == 0) {
+                    if (!addedLast) {
+                        dc++;
+                        res.append(list.get(dc - 1));
+                        dc--;
+                        addedLast = true;
+                    }
+                    res.append(list.get(dc - 1));
+                    dc--;
+                } else {
+                    int j = i;
+                    while (j < pattern.length() && pattern.charAt(j) == 'D') {
+                        j++;
+                    }
+                    res.append(list.remove(j - i));
+                    dc--;
                 }
             }
         }
-        
-        return result.toString();
+
+        return res.toString();
     }
 }
