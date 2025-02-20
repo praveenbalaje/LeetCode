72d5fb01831a21617e539ae7a5973455a01439d1
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-02-20 07:54:52 +0530

    Time: 8 ms (20.1%), Space: 42.6 MB (16.97%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.java
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.java
@@ -1,27 +1,22 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         ArrayList<String> res = new ArrayList<>();
-        List<String> num =  Arrays.asList(nums);
-        System.out.println(num);
-        permutate(nums[0].length(), "" , res,num);
-        System.out.println(res);
+        List<String> num = Arrays.asList(nums);
+        permutate(nums[0].length(), "", res, num);
         return res.get(0);
     }
 
-    private void permutate(int n,String cur, ArrayList<String> res,List<String> num) {
-        System.out.println(num + " cur :  " + cur);
-        if(cur.length() == n) {
-            if(!num.contains(cur)){
+    private void permutate(int n, String cur, ArrayList<String> res, List<String> num) {
+        if (cur.length() == n) {
+            if (!num.contains(cur)) {
                 res.add(cur);
             }
             return;
         }
-         for (char ch : new char[]{'0', '1'}) {
-            // if (cur.isEmpty() ) {
-                if(res.size() == 0) {
-                permutate(n, cur + ch, res,num);
-                }
-            // }
+        for (char ch : new char[] { '0', '1' }) {
+            if (res.size() == 0) {
+                permutate(n, cur + ch, res, num);
+            }
         }
     }
 }
