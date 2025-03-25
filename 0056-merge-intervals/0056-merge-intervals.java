b649c8cbc2317fc5e4654adb5a76bd80ec855185
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-03-25 11:16:33 +0530

    Time: 95 ms (5.43%), Space: 48 MB (6.8%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]); // Sort by end time
+            }
+            return Integer.compare(a[0], b[0]); // If end times are equal, sort by start time
+        });
+        for (int i = 0; i < intervals.length; i++) {
+            System.out.println(intervals[i][0] + "  " + intervals[i][1]);
+        }
+
+        int[][] res = new int[intervals.length][2];
+        int count = 0;
+        res[count][0] = intervals[0][0];
+        res[count][1] = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            System.out.println("loop" + intervals[i][0] + " : " +  intervals[count][1]);
+            if (intervals[i][0] <= res[count][1]) {
+                if(intervals[i][1] > res[count][1]) {
+                res[count][1] = intervals[i][1];
+                }
+            } else {
+                count += 1;
+                res[count][0] = intervals[i][0];
+                res[count][1] = intervals[i][1];
+            }
+        }
+
+        int ans[][] = new int[count + 1][2];
+        for (int i = 0; i <= count; i++) {
+            ans[i][0] = res[i][0];
+            ans[i][1] = res[i][1];
+        }
+
+        return ans;
+    }
+}
