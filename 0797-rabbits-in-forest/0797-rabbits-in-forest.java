ee96b235b62e57d40e99da2dfa5aadd887032fdd
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-04-20 15:26:54 +0530

    Time: 3 ms (82.48%), Space: 42.1 MB (90.69%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -3,19 +3,17 @@ class Solution {
         int counter = 0;
         Map<Integer, Integer> mp = new HashMap<>();
         for (int i = 0; i < answers.length; i++) {
-            int value = mp.getOrDefault(answers[i], 0) + 1;
-            mp.put(answers[i], value);
+            mp.put(answers[i], mp.getOrDefault(answers[i], 0) + 1);
         }
 
         for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
             int value = entry.getKey();
             int count = entry.getValue();
             counter += count;
-            if ((count % (value + 1) != 0)) {
-                counter += (value + 1) - (count % (value + 1));
+            int remainder = count % (value + 1);
+            if (remainder != 0) {
+                counter += (value + 1) - remainder;
             }
-            System.out.println(
-                    "value : " + value + " count  : " + count + " mod : " + ((value + 1) - (count % (value + 1))));
         }
         return counter;
     }
