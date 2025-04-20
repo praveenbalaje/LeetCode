3398960c4708628edf4e4f2586265e29c04fbadf
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-04-20 15:29:44 +0530

    Time: 3 ms (82.48%), Space: 42.3 MB (70.99%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -1,25 +1,20 @@
 class Solution {
     public int numRabbits(int[] answers) {
-        int totalRabbits = 0;
-        Map<Integer, Integer> answerCountMap = new HashMap<>();
-
-        // Count how many times each answer appears
-        for (int answer : answers) {
-            answerCountMap.put(answer, answerCountMap.getOrDefault(answer, 0) + 1);
+        int counter = 0;
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < answers.length; i++) {
+            mp.put(answers[i], mp.getOrDefault(answers[i], 0) + 1);
         }
 
-        // Calculate the total number of rabbits
-        for (Map.Entry<Integer, Integer> entry : answerCountMap.entrySet()) {
-            int x = entry.getKey(); // the answer given by a rabbit
-            int count = entry.getValue(); // how many rabbits gave this answer
-            int groupSize = x + 1;
-
-            // Number of groups needed = ceil(count / groupSize)
-            int groups = (count + x) / groupSize;
-
-            totalRabbits += groups * groupSize;
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            int value = entry.getKey();
+            int count = entry.getValue();
+            counter += count;
+            int remainder = count % (value + 1);
+            if (remainder != 0) {
+                counter += (value + 1) - remainder;
+            }
         }
-
-        return totalRabbits;
+        return counter;
     }
 }
