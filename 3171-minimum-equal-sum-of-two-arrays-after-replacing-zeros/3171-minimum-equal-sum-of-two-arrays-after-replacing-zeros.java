f7bc5fd30b3cd68a577e57fd520adf5a99976625
praveenbalaje <44121802+praveenbalaje@users.noreply.github.com>
2025-05-10 16:41:12 +0530

    Time: 3 ms (88.73%), Space: 61.4 MB (8.92%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -1,42 +1,43 @@
 class Solution {
     public long minSum(int[] nums1, int[] nums2) {
-        long s1 = 0;
-        long s2 = 0;
-        long z1 = 0 ;
-        long z2 = 0;
-        for(int i=0;i<nums1.length;i++) {
+        long s1 = 0, s2 = 0;
+        long z1 = 0, z2 = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
             s1 += nums1[i];
-            if(nums1[i] == 0) z1++;
+            if (nums1[i] == 0) z1++;
         }
 
-        for(int i=0;i<nums2.length;i++) {
+        for (int i = 0; i < nums2.length; i++) {
             s2 += nums2[i];
-            if(nums2[i] == 0) z2++;
+            if (nums2[i] == 0) z2++;
         }
 
+        long zeroMax = Math.max(z1, z2);
 
-        long zeroMax = Math.max(z1,z2);
-        if(s1 == s2 && z1 == 0 && z2 == 0) return s1;
-        else if(s1 > s2) {
+        if (s1 == s2 && z1 == 0 && z2 == 0) {
+            return s1;
+        } else if (s1 > s2) {
             long diff = s1 - s2;
-            if(z2 == 0 || (z1 == 0 && diff < z2)) {
+
+            if (z2 == 0 || (z1 == 0 && diff < z2)) {
                 return -1;
-            } else if(diff < zeroMax) {
-                return Math.max(s1+z1,s2+z2);
+            } else if (diff < zeroMax) {
+                return Math.max(s1 + z1, s2 + z2);
             } else {
                 return s1 + z1;
             }
+
         } else {
             long diff = s2 - s1;
-            if(z1 == 0 || (z2 == 0 && diff < z1)) {
+
+            if (z1 == 0 || (z2 == 0 && diff < z1)) {
                 return -1;
-            } else if(diff < zeroMax) {
-                return Math.max(s1+z1,s2+z2);
-            }else {
+            } else if (diff < zeroMax) {
+                return Math.max(s1 + z1, s2 + z2);
+            } else {
                 return s2 + z2;
             }
         }
-
-        
     }
 }
